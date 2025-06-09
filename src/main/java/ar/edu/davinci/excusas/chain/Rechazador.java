5c1234a4ef468fd2dc6f58d15d04abae71819acd
jcarbajal-dv <juan.carbajal@davinci.edu.ar>
2025-06-08 22:12:09 -0300

    codigo still roto

--- src/main/java/ar/edu/davinci/excusas/chain/Rechazador.java
+++ src/main/java/ar/edu/davinci/excusas/chain/Rechazador.java
@@ -1,4 +1,36 @@
 package ar.edu.davinci.excusas.chain;
 
+import ar.edu.davinci.excusas.model.Excusa;
+
 public class Rechazador extends Encargado{
+    public Rechazador(){}
+
+    public Encargado getSiguiente(){
+        return null;
+    }
+
+    public void procesar(Excusa excusa){
+        System.out.println("Necesitamos mas pruebas");
+
+    }
+
+    public boolean soyCEO(){
+        return false;
+    }
+
+    public boolean soyRecepcionista(){
+        return false;
+    }
+
+    public boolean soyGerenteRRHH(){
+        return false;
+    }
+
+    public boolean soySupervisorArea(){
+        return false;
+    }
+
+    public boolean soyEncargadoDefault(){
+        return false;
+    }
 }
